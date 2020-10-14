c0a048fc9ab095c4b091bfcd85dde29cc259c567
Khoale1096 <lexxx611@umn.edu>
2020-10-13 22:05:33 -0400

    First commit to dockerize scene detection model

--- server/model.py
+++ server/model.py
@@ -1,5 +1,7 @@
 import asyncio
 
+from SceneDetection import scene_detec_model
+
 async def init():
     """
     This method will be run once on startup. You should check if the supporting files your
@@ -14,6 +16,8 @@ def predict(image_file):
     Interface method between model and server. This signature must not be
     changed and your model must be able to predict given this input
     """
+    scene_detect_result = scene_detec_model.get_scene_attributes(image_file)
     return {
-        "someResultCategory": "actualResultValue",
+        "Category Confidence": scene_detect_result['category_results'],
+        "Scene Attributes": scene_detect_result['attributes_result'],
     }
