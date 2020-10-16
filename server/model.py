e0cf36c4c73df5dd814fe585edadab3455d119c8
Hannah Lerner <hlerner@Hannahs-MacBook-Pro.local>
2020-10-15 23:22:24 -0400

    work

--- server/model.py
+++ server/model.py
@@ -1,158 +1,80 @@
 import asyncio
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import argparse
-import json
-import os
-from os import listdir
-from os.path import isfile, join
-import csv
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data.dataloader import default_collate 
 
-from torchvision import transforms
-from PIL import Image
+from pathlib import Path
+import shutil
+import pandas as pd
 
-torch.backends.cudnn.deterministic = True
+from model import get_model
+from classify_faces_dataset import CustomFaceAgeClassificationDataset
+import time
 
-##########################
-# SETTINGS
-##########################
-
-# Hyperparameters
-learning_rate = 0.0005
-num_epochs = 200
-
-# Architecture
-NUM_CLASSES = 4
-BATCH_SIZE = 256
-GRAYSCALE = False
-
-##########################
-# MODEL
-##########################
-DEVICE = torch.device('cpu')
-
-def conv3x3(in_planes, out_planes, stride=1):
-    """3x3 convolution with padding"""
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
-                     padding=1, bias=False)
+def file_path_collate(batch):
+    # Custom collate function for dataloader that allows
+    # for the dataloader to process a batch of Path items
+    new_batch = []
+    file_paths = []
+    for _batch in batch:
+        new_batch.append(_batch[:-1])
+        file_paths.append(_batch[-1])
+    return default_collate(new_batch), file_paths
 
 
-class BasicBlock(nn.Module):
-    expansion = 1
+def classify_faces(model_path=None, unclassified_faces_path=None, batch_size=32):
+    # Set testing device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(BasicBlock, self).__init__()
-        self.conv1 = conv3x3(inplanes, planes, stride)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3x3(planes, planes)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.downsample = downsample
-        self.stride = stride
+    # Set up Datasets
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
-    def forward(self, x):
-        residual = x
+    # images_root = Path(unclassified_faces_path)
+    # test_dataset = CustomFaceAgeClassificationDataset(imgs_path=images_root, transform=transform)
 
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
+    # # Set up dataloaders
+    # test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size,
+                                            # shuffle=False, num_workers=0, collate_fn=file_path_collate)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
+    # Initialize model
+    model = get_model()
+    model.load_state_dict(torch.load(Path(model_path)))
+    model = model.to(device)
+    model.eval()
 
-        if self.downsample is not None:
-            residual = self.downsample(x)
+    # Process images for predictions, then move them to predicted label folders
+    # Label translation 
+    label_translation = {
+        0: "<= 12",
+        1: "13 to 17",
+        2: ">= 18"
+    }
 
-        out += residual
-        out = self.relu(out)
+    # Set up output sorted folders
+    # sorted_faces_folder = images_root.parent.joinpath('classified_faces')
+    # faces_12_or_less = sorted_faces_folder.joinpath('12_or_less')
+    # faces_13_to_17 = sorted_faces_folder.joinpath('13_to_17')
+    # faces_18_and_above = sorted_faces_folder.joinpath('18_and_above')
 
-        return out
+    # Path(faces_12_or_less).mkdir(parents=True, exist_ok=True)
+    # Path(faces_13_to_17).mkdir(parents=True, exist_ok=True)
+    # Path(faces_18_and_above).mkdir(parents=True, exist_ok=True)
+    # Get input data and corresponding filepaths
+    inputs = inputs[0]
+    inputs = inputs.to(device)
 
+    # Forward 
+    outputs = model(inputs)
+    predicted = torch.argmax(outputs, dim=1)
 
-class ResNet(nn.Module):
-
-    def __init__(self, block, layers, num_classes, grayscale):
-        self.num_classes = num_classes
-        self.inplanes = 64
-        if grayscale:
-            in_dim = 1
-        else:
-            in_dim = 3
-        super(ResNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_dim, 64, kernel_size=7, stride=2, padding=3,
-                               bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.relu = nn.ReLU(inplace=True)
-        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        self.avgpool = nn.AvgPool2d(7, stride=1, padding=2)
-        self.fc = nn.Linear(2048 * block.expansion, 1, bias=False)
-        self.linear_1_bias = nn.Parameter(torch.zeros(self.num_classes-1).float())
-
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, (2. / n)**.5)
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
-    def _make_layer(self, block, planes, blocks, stride=1):
-        downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
-
-        layers = []
-        layers.append(block(self.inplanes, planes, stride, downsample))
-        self.inplanes = planes * block.expansion
-        for i in range(1, blocks):
-            layers.append(block(self.inplanes, planes))
-
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-        x = self.maxpool(x)
-
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        logits = self.fc(x)
-        logits = logits + self.linear_1_bias
-        probas = torch.sigmoid(logits)
-        return logits, probas
-
-
-def resnet34(num_classes, grayscale):
-    """Constructs a ResNet-34 model."""
-    model = ResNet(block=BasicBlock,
-                   layers=[3, 4, 6, 3],
-                   num_classes=num_classes,
-                   grayscale=grayscale)
-    return model
-#######################
-### Initialize Model
-#######################
-
-model = resnet34(NUM_CLASSES, GRAYSCALE)
-print('test')
-# model.load_state_dict(torch.load('model/model.pt', map_location=DEVICE))
-model.eval()
+    return predicted
 
 async def init():
     await asyncio.sleep(2)
@@ -164,26 +86,15 @@ async def init():
 
 def predict(image_file):
     print('image file:', image_file.name)
-    image = Image.open(image_file.name, mode='r')
-    custom_transform = transforms.Compose([transforms.Resize((128, 128)),transforms.CenterCrop((120, 120)),transforms.ToTensor()])
-    image = custom_transform(image)
-    #DEVICE = torch.device('cpu')
-    image = image.to(DEVICE)
-    image = image.unsqueeze(0)
-    with torch.set_grad_enabled(False):
-        logits, probas = model(image)
-        predict_levels = probas > 0.5
-        predicted_label = torch.sum(predict_levels, dim=1)
-        predicted_age=predicted_label.item()
-        ret_val = 'unknown'
-        if predicted_age == 0:
-            ret_val = 'below 9'
-        elif predicted_age == 1:
-            ret_val = '9 - 13'
-        elif predicted_age == 2:
-            ret_val = '14 - 17'
-        else:
-            ret_val = '18 or above 18'
+    model_path = 'three_class_trained_age_recognition_model.pth'
+    unclassified_faces_path = image_file.name
+    # Classify faces
+    start = time.time()
+    classify_faces(model_path=model_path, unclassified_faces_path=unclassified_faces_path)
+    end = time.time()
+    print(end - start)
+
+    print('Finished!')
     return { "predicted age": ret_val,}
 
 
